4bcd180453175df05c082a372209de2ea27742ff
mmawyin <mmawyin@telconet.ec>
2023-03-10 18:22:25 -0500

    refactoring inputparser

--- src/utils/inputParser.js
+++ src/utils/inputParser.js
@@ -6,30 +6,44 @@ class InputParser {
 
     parseInputFile(inputFilePath) {
         const fileContent = fs.readFileSync(inputFilePath, 'utf-8');
-        const employeeDataArray = fileContent.replace('\r','').split('\n');
-        
-        const employees = employeeDataArray.map(employeeData => {
-            employeeData=employeeData.replace('\r','');
-            const [name, scheduleData] = employeeData.split('=');
-            
-            const scheduleArray = scheduleData.split(',');
-            
-            const schedules = scheduleArray.map(scheduleData => {
-
-                const day = scheduleData.substring(0, 2);
-                const timeRange = scheduleData.substring(2);
-                const [startTime, endTime] = timeRange.split('-').map(time => {
-                    const [hours, minutes] = time.split(':');
-                    return new Date().setHours(hours, minutes);
-                });
-
-                return new Schedule(day, startTime, endTime);
-            });
-            console.log(schedules);
-            return new Employee(name, schedules);
+        const employeeDataArray = this.extractEmployeeData(fileContent);
+    
+        const employees = employeeDataArray.map((employeeData) => {
+          employeeData = employeeData.replace('\r', '');
+    
+          const [name, scheduleData] = this.extractNameAndScheduleData(employeeData);
+          const schedules = this.extractSchedules(scheduleData);
+    
+          return new Employee(name, schedules);
         });
-
+    
         return employees;
+      }
+
+    extractEmployeeData(employeeData) {
+        return employeeData.replace('\r', '').split('\n');
+    }
+    extractNameAndScheduleData(employeeData) {
+        return employeeData.split('=');
+    }
+
+    extractSchedules(scheduleData) {
+        const scheduleArray = scheduleData.split(',');
+        return scheduleArray.map(scheduleData => {
+            const day = scheduleData.substring(0, 2);
+            const timeRange = scheduleData.substring(2);
+            const [startHour, endHour] = timeRange.split('-').map(time => {
+                const [hours, minutes] = time.split(':');
+                return new Date().setHours(hours, minutes,0,0);
+            });
+            return new Schedule(day, startHour, endHour);
+        });
+    }
+    extractTimeRange(timeRange) {
+        return timeRange.split('-').map(time => {
+            const [hours, minutes] = time.split(':');
+            return new Date().setHours(hours, minutes,0,0);
+        });
     }
 }
 
---
+++ test/utils/inputParser.test.js
@@ -0,0 +1,62 @@
+const InputParser = require('../../src/utils/InputParser');
+const testInputPath = 'input/test_data.txt';
+describe('InputParser', () => {
+  describe('parseInputFile', () => {
+    it('should parse input file and return an array of employees', () => {
+      const parser = new InputParser();
+      const employees = parser.parseInputFile(testInputPath);
+      expect(employees).toHaveLength(2);
+      expect(employees[0].name).toEqual('ALLISON');
+      expect(employees[0].schedules).toHaveLength(4);
+      expect(employees[1].name).toEqual('JOHN');
+      expect(employees[1].schedules).toHaveLength(3);
+    });
+  });
+
+  describe('extractEmployeeData', () => {
+    it('should extract employee data from a string', () => {
+      const parser = new InputParser();
+      const employeeData = 'ALLISON=TH08:00-12:00,TH13:00-17:00,FR08:00-12:00,FR13:00-17:00';
+      const extractedEmployeeData = parser.extractEmployeeData(employeeData);
+      expect(extractedEmployeeData).toEqual([
+        'ALLISON=TH08:00-12:00,TH13:00-17:00,FR08:00-12:00,FR13:00-17:00'
+      ]);
+    });
+  });
+
+  describe('extractNameAndScheduleData', () => {
+    it('should extract name and schedule data from an employee string', () => {
+      const parser = new InputParser();
+      const employeeData = 'ALLISON=TH08:00-12:00,TH13:00-17:00,FR08:00-12:00,FR13:00-17:00';
+      const [name, scheduleData] = parser.extractNameAndScheduleData(employeeData);
+      expect(name).toEqual('ALLISON');
+      expect(scheduleData).toEqual('TH08:00-12:00,TH13:00-17:00,FR08:00-12:00,FR13:00-17:00');
+    });
+  });
+
+  describe('extractSchedules', () => {
+    it('should extract schedules from a schedule data string', () => {
+      const parser = new InputParser();
+      const scheduleData = 'TH08:00-12:00,TH13:00-17:00,FR08:00-12:00,FR13:00-17:00';
+      const schedules = parser.extractSchedules(scheduleData);
+      expect(schedules).toHaveLength(4);
+      expect(schedules[0].day).toEqual('TH');
+      expect(schedules[0].startHour).toEqual(new Date().setHours(8, 0,0,0));
+      expect(schedules[0].endHour).toEqual(new Date().setHours(12, 0,0,0));
+      expect(schedules[3].day).toEqual('FR');
+      expect(schedules[3].startHour).toEqual(new Date().setHours(13, 0,0,0));
+      expect(schedules[3].endHour).toEqual(new Date().setHours(17, 0,0,0));
+    });
+  });
+
+  describe('extractTimeRange', () => {
+    it('should extract start and end time from a time range string', () => {
+      const parser = new InputParser();
+      const timeRange = '08:00-12:00';
+      const [startHour, endHour] = parser.extractTimeRange(timeRange);
+      expect(startHour).toEqual(new Date().setHours(8, 0,0,0));
+      expect(endHour).toEqual(new Date().setHours(12, 0,0,0));
+    });
+ 
+  });
+});
